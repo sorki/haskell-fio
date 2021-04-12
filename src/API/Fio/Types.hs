cda148fc520f3c8a5c659aa395f054ec8a836759
Richard Marko <srk@48.io>
2021-04-12 17:06:29 +0200

    re-export Token from Types

--- src/API/Fio/Types.hs
+++ src/API/Fio/Types.hs
@@ -2,8 +2,10 @@
 module API.Fio.Types
   ( module API.Fio.Types.AccountStatement
   , module API.Fio.Types.Payment
+  , module API.Fio.Types.Token
   ) where
 
 import API.Fio.Types.AccountStatement
 import API.Fio.Types.Payment
+import API.Fio.Types.Token
 
